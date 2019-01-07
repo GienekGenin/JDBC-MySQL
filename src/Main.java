0d842d8f99da30a5b0b9601caa681e9386d2e3de
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2019-01-07 19:36:24 +0100

    Retrieving data from table

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
@@ -12,6 +10,14 @@ public class Main {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "admin");
+
+            Statement statement = con.createStatement();
+            ResultSet set = statement.executeQuery("SELECT * FROM mydb.users");
+            while (set.next()){
+                String name = set.getString("name");
+                System.out.println(name);
+            }
+
             System.out.println("DB connection success");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
