b99f2725e09711da274a7983318d11e6977a63d0
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2019-01-07 21:08:15 +0100

    Blobs and imgs

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.*;
 
 public class Main {
@@ -10,14 +15,30 @@ public class Main {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "admin");
+            Statement statement = con.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Books (id MEDIUMINT NOT NULL AUTO_INCREMENT, name CHAR(30) NOT NULL, img BLOB, PRIMARY KEY (id))");
 
-            String param = "Pasha";
-            PreparedStatement preparedStatement = con.prepareStatement("SELECT * FROM mydb.users where name = ?");
-            preparedStatement.setString(1, param);
-            ResultSet set  = preparedStatement.executeQuery();
+            try {
+                BufferedImage image = ImageIO.read(new File("D:\\photos\\rr.jpg"));
+                Blob blob = con.createBlob();
+                try (OutputStream outputStream = blob.setBinaryStream(1)){
+                    ImageIO.write(image,"jpg", outputStream);
+                }
+                PreparedStatement statement1 = con.prepareStatement("INSERT INTO Books (NAME , img) VALUES (?,?)");
+                statement1.setString(1,"test");
+                statement1.setBlob(2, blob);
+                statement1.execute();
 
-            while (set.next()){
-                System.out.println(set.getString("name"));
+                ResultSet set = statement.executeQuery("SELECT * FROM Books");
+                while (set.next()){
+                    Blob blob1 = set.getBlob("img");
+                    BufferedImage image1 = ImageIO.read(blob1.getBinaryStream());
+                    File outputFile = new File("saved.jpg");
+                    ImageIO.write(image1, "jpg", outputFile);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
             System.out.println("DB connection success");
