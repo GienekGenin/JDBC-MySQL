3d6361591809caeace0aac14e86ea47db85c45bc
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2019-01-07 20:39:47 +0100

    Preparement statent. SQL injections defence

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,13 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "admin");
 
-            Statement statement = con.createStatement();
-            ResultSet set = statement.executeQuery("SELECT * FROM mydb.users");
+            String param = "Pasha";
+            PreparedStatement preparedStatement = con.prepareStatement("SELECT * FROM mydb.users where name = ?");
+            preparedStatement.setString(1, param);
+            ResultSet set  = preparedStatement.executeQuery();
+
             while (set.next()){
-                String name = set.getString("name");
-                System.out.println(name);
+                System.out.println(set.getString("name"));
             }
 
             System.out.println("DB connection success");
